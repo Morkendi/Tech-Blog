085c78d8e921e863f8e59f05c00c54e09934de59
Daniel Sanchez <morkendi@gmail.com>
2023-07-05 17:36:12 -0600

    Fix Sequelize Syntax Error

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 // GET all users
 router.get('/', async (req, res) => {
